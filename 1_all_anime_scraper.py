b47441932554735e9620caeff0311cb30177d3fc
EruditeCoder <marcosaleman84@gmail.com>
2022-12-11 00:15:44 -0500

    (scrape_all_anime): completed

--- 1_all_anime_scraper.py
+++ 1_all_anime_scraper.py
@@ -1,36 +1,61 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import time
 
-# Set up the webdriver
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--user-agent=MyUserAgent')
-driver = webdriver.Chrome(chrome_options=chrome_options)
+anime = {}
 
-# Request the HTML page and wait for it to load
-driver.get('https://www.crunchyroll.com/videos/alphabetical')
-wait = WebDriverWait(driver, 180)
-wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'erc-browse')))
+def print_anime_info(anime: dict):
+  # iterate over the dictionary
+  for key, value in anime.items():
+    print(key + ' || ' + value)
 
-cards = []
+def save_to_file(anime: dict):
+  with open('anime.csv', 'w') as f:
+    f.write('title,url' + '\n')
+    
+    for key,value in anime.items():
+      f.write(key + ',' + value + '\n')
 
-while True:
-  driver.execute_script("window.scrollBy(0, 140);")
-  # wait for the page to load
-  time.sleep(2)
+def scrape_anime():
+  # Set up the webdriver
+  chrome_options = webdriver.ChromeOptions()
+  chrome_options.add_argument('--user-agent=MyUserAgent')
+  driver = webdriver.Chrome(chrome_options=chrome_options)
 
-  wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'horizontal-card-static__link--q268E')))
-  elems = driver.find_element(By.XPATH, '//a[@class="horizontal-card-static__link--q268E"]')
-  
-  # obtain all attributes of elems
-  print(elems.get_attribute('title'))
-  print(elems.get_attribute('href'))
+  # Request the HTML page and wait for it to load
+  driver.get('https://www.crunchyroll.com/videos/alphabetical')
+  wait = WebDriverWait(driver, 180)
+  wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'erc-browse')))
 
-# Print the number of horizontal cards found
-print(f'Found {len(horizontal_cards)} horizontal cards')
+  while True:
+    driver.execute_script("window.scrollBy(0, 200)")
+    # wait for the page to load
+    time.sleep(0.1)
 
-# Close the webdriver
-driver.close()
+    wait.until(EC.presence_of_all_elements_located(
+      (By.CLASS_NAME, 'horizontal-card-static__link--q268E')))
+    
+    elems = driver.find_element(
+      By.XPATH, '//a[@class="horizontal-card-static__link--q268E"]')
+    
+    title = elems.get_attribute('title')
+    href = elems.get_attribute('href')
+
+    anime[title] = href
+
+    # check if no more scrolling is possible
+    if driver.execute_script("return document.body.scrollHeight") == driver.execute_script("return window.innerHeight + window.scrollY"):
+      break
+
+  print('finished scraping')
+
+  # Close the webdriver
+  driver.close()
+
+scrape_anime()
+
+# print_anime_info(anime)
+
+save_to_file(anime)
