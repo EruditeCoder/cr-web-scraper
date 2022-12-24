888fb58bea8b24057b3f18757e8eaa1209985240
EruditeCoder <marcosaleman84@gmail.com>
2022-12-24 15:09:42 -0500

    (WIP): scraping anime per url

--- 2_scrape_each_anime.py
+++ 2_scrape_each_anime.py
@@ -1,5 +1,6 @@
 import csv
 import time
+import os
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
@@ -8,13 +9,41 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def scrape_anime():
-    # Open the CSV file and read the titles and URLs
-    with open('anime.csv', 'r') as csv_file:
-        reader = csv.reader(csv_file)
-        next(reader)
-        data = list(reader)
-
+    data = []
     ratings = []
+
+    ratings_cols = 'title,rating,rating_count'
+
+    last_line = False
+
+    if os.path.exists('ratings.csv'):
+        # get last line of csv
+        with open('ratings.csv', 'r') as f:
+            for line in f:
+                if line.strip() and line.strip() != ratings_cols:
+                    last_line = line
+    else:
+        with open('ratings.csv', 'w') as csv_file:
+            csv_file.write(ratings_cols + '\n')
+
+    with open('anime.csv', 'r', encoding='utf-8') as csv_file:
+        catched_up = False
+
+        for line in csv_file.readlines():
+            if line.__contains__('title,url'):
+                continue
+
+            if last_line and line.__contains__(last_line.split(',')[0]):
+                catched_up = True
+                continue
+
+            if catched_up or not last_line:
+                title, url = line.rsplit(',', 1)
+
+                title = title.strip()
+                url = url.strip()
+
+                data.append([title, url])
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--user-agent=MyUserAgent')
@@ -23,8 +52,15 @@ def scrape_anime():
     for title, url in data:
         driver.get(url)
         wait = WebDriverWait(driver, 180)
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME,
-                                                   'content-image__figure--7vume')))
+
+        try:
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME,
+                                                       'content-image__figure--7vume')))
+        except NoSuchElementException or TimeoutError:
+            time.sleep(3)
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME,
+                                                       'content-image__figure--7vume')))
+            pass
 
         try:
             mature_info_wrapper = driver.find_element(By.CLASS_NAME, 'mature-info-wrapper')
@@ -41,7 +77,6 @@ def scrape_anime():
                                              '//span[@class="text--gq6o- text--is-heavy--2YygX text--is-m--pqiL- '
                                              'star-rating-average-data__label--TdvQs"]')
 
-        print(rating_element.text)
         rating_text = rating_element.text
 
         if rating_text.__contains__('—'):
@@ -57,12 +92,8 @@ def scrape_anime():
 
         print("{}: {} ({})".format(title, rating, rating_count))
 
-        ratings.append((title, rating, rating_count))
-
-    with open('ratings.csv', 'w') as csv_file:
-        csv_file.write('title,rating,rating_count' + '\n')
-        writer = csv.writer(csv_file)
-        writer.writerows(ratings)
+        with open('ratings.csv', 'a') as csv_file:
+            csv_file.write(title + ',' + rating + ',' + rating_count + '\n')
 
     driver.quit()
 
