41928bfec7c92e4dd5fdb89db341a25cbde5b6b0
EruditeCoder <marcosaleman84@gmail.com>
2022-12-26 13:35:05 -0500

    removed unnecessary comments

--- 3_order.py
+++ 3_order.py
@@ -1,19 +1,13 @@
-# Open the ratings.csv file and read its contents into a list of strings
 with open('ratings.csv', 'r') as f:
     ratings = f.readlines()
 
 
-# Sort the list of strings by the second element (the rating) using the sorted() function
-# and a custom key function
 def rating_key(rating_str):
-    # Split the string on the comma to get a list of elements
     elements = rating_str.rsplit(',')
-    # Return the second element (the rating) as the key
     return elements[-2]
 
 
 sorted_ratings = sorted(ratings, key=rating_key, reverse=True)
 
-# Open a new file called sorted_ratings.csv and write the sorted list of strings to this file
 with open('sorted_ratings.csv', 'w') as f:
     f.writelines(sorted_ratings)
